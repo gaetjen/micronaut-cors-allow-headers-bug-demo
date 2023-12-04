2408dd7152642a18a8fff206503e79834bba8969
Johannes Gätjen <johannesgaetjen@gmail.com>
2023-12-04 04:22:19 +0100

    failing example

---
+++ src/main/kotlin/example/micronaut/util/model/IncludeType.kt
@@ -0,0 +1,8 @@
+package example.micronaut.util.model
+
+enum class IncludeType(val stringValue: String) {
+    Permissions("permissions"),
+    PushAlias("pushAlias"),
+    ChargePoints("chargePoints"),
+    ;
+}
